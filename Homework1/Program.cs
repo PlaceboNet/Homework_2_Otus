82353a434de29dc3f483fdb32c86d457a6efa84e
PlaceboNet <mileninane@yandex.ru>
2025-09-05 21:05:30 +0300

    Редактирование д/з

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -5,53 +5,49 @@ namespace Homework1
     internal class Program
     {
         private static string massege;
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Привет!" +
-                "\nЯ могу выполнить несколько команд:" +
+        private static string iCanDo = "\nЯ могу выполнить несколько команд:" +
                 "\n/start - программа просит ввести имя" +
                 "\n/help - краткая справочная информация о том, как пользоваться программой" +
                 "\n/info - предоставляет информацию о версии программы и дате её создания" +
                 "\n/echo - после ввода имени данная команда становится доступной" +
-                "\n/exit - выйти из программы");
+                "\n/exit - выйти из программы";
+        private static string info = "Версия 0.0.1\n27.08.2025";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Привет! {iCanDo}");
 
             while (true)
             {
                 Console.Write("Введите команду: ");
                 string input = Console.ReadLine();
+                string echo = string.Empty;
+                if (input.Contains(" "))
+                {
+                    echo = input.Split(" ")[1];
+                    input = input.Split(" ")[0];
+                }
 
                 if (input == "/exit") break;
 
                 switch (input)
                 {
                     case "/start":
-                        var instance = new Program();
-                        instance.StartMessage();
+                        StartMessage();
                         break;
                     case "/help":
                         if (!string.IsNullOrEmpty(massege))
-                            Console.WriteLine($"Я с удовольствием напомню тебе, {massege}, что я могу сделать" +
-                "\n/start - программа просит ввести имя" +
-                "\n/help - краткая справочная информация о том, как пользоваться программой" +
-                "\n/info - предоставляет информацию о версии программы и дате её создания" +
-                "\n/echo - после ввода имени данная команда становится доступной" +
-                "\n/exit - выйти из программы");
-                        else Console.WriteLine($"Я с удовольствием напомню тебе, что я могу сделать" +
-                "\n/start - программа просит ввести имя" +
-                "\n/help - краткая справочная информация о том, как пользоваться программой" +
-                "\n/info - предоставляет информацию о версии программы и дате её создания" +
-                "\n/echo - после ввода имени данная команда становится доступной" +
-                "\n/exit - выйти из программы");
+                            Console.WriteLine($"Я с удовольствием напомню тебе, {massege}, что я могу сделать {iCanDo}");
+                        else Console.WriteLine($"Я с удовольствием напомню тебе, что я могу сделать {iCanDo}");
                         break;
                     case "/info":
                         if (!string.IsNullOrEmpty(massege))
-                            Console.WriteLine($"{massege},\nВерсия 0.0.1\n27.08.2025");
-                        else Console.WriteLine("Версия 0.0.1\n27.08.2025");
+                            Console.WriteLine($"{massege},{info}");
+                        else Console.WriteLine(info);
                         break;
                     case "/echo":
                         if (!string.IsNullOrEmpty(massege))
-                            Console.WriteLine($"Привет, {massege}");
+                            Console.WriteLine(echo);
                         else
                             Console.WriteLine("Сначала представьтесь командой '/start'");
                         break;
@@ -62,8 +58,10 @@ namespace Homework1
             }
         }
 
-        public void StartMessage()
+        public static void StartMessage()
         {
+            if(!string.IsNullOrEmpty(massege))
+                return;
             Console.WriteLine("Введите имя:");
             massege = Console.ReadLine();
             Console.WriteLine($"Привет, {massege}");
