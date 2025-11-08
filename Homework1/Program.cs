d864dffcf0c69f64eacd7605a8c0cf17a1c08f06
PlaceboNet <53406734+PlaceboNet@users.noreply.github.com>
2025-11-08 18:17:55 +0300

    Revert "Интерфейсы"

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,82 +1,70 @@
-﻿using Microsoft.VisualBasic;
-using Otus.ToDoList.ConsoleBot;
-using Otus.ToDoList.ConsoleBot.Types;
-using System;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using static Homework1.ToDoItem;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System;
 
 namespace Homework1
 {
     internal class Program
     {
+        private static string massege;
         private static string iCanDo = "\nЯ могу выполнить несколько команд:" +
-            "\n/start - программа просит ввести имя" +
-            "\n/help - краткая справочная информация о том, как пользоваться программой" +
-            "\n/info - предоставляет информацию о версии программы и дате её создания" +
-            "\n/exit - выйти из программы" +
-            "\n/addtask - добавить новую задачу в список" +
-            "\n/showtasks - отобразить список всех добавленных задач" +
-            "\n/removetask - удалять задачи по номеру в списке" +
-            "\n/completetask - найти задачу по id" +
-            "\n/showalltasks - выводить команды с любым State\n";
+                "\n/start - программа просит ввести имя" +
+                "\n/help - краткая справочная информация о том, как пользоваться программой" +
+                "\n/info - предоставляет информацию о версии программы и дате её создания" +
+                "\n/echo - после ввода имени данная команда становится доступной" +
+                "\n/exit - выйти из программы";
         private static string info = "Версия 0.0.1\n27.08.2025";
-        public static string separation = "---------------------------";
-        public static int MaxTask;
-        public static int MaxLength;
 
-        static void Main(Update update, ConsoleBotClient botClient)
+        static void Main(string[] args)
         {
-            do
+            Console.WriteLine($"Привет! {iCanDo}");
+
+            while (true)
             {
-                try
+                Console.Write("Введите команду: ");
+                string input = Console.ReadLine();
+                string echo = string.Empty;
+                if (input.Contains(" "))
                 {
-                    Console.WriteLine($"Привет! {iCanDo}");
-                    Console.WriteLine("Введите максимально допустимое количество задач");
-                    MaxTask = Convert.ToInt32(Console.ReadLine());
-                    if (MaxTask > 100 || MaxTask < 1)
-                    {
-                        throw new ArgumentException("Максимальное количество задач должно быть числом от 1 до 100.");
-                    }
-                    Console.WriteLine("Введите максимально допустимую длину задачи");
-                    MaxLength = Convert.ToInt32(Console.ReadLine());
-                    if (MaxLength > 100 || MaxLength < 1)
-                    {
-                        throw new ArgumentException("Максимально допустимая длина задачи должно быть числом от 1 до 100.");
-                    }
-                    Console.Write("Введите команду: ");
-                    var input = Console.ReadLine();
+                    echo = input.Split(" ")[1];
+                    input = input.Split(" ")[0];
+                }
 
-                    if (string.IsNullOrEmpty(input))
-                    {
-                        Console.WriteLine("Пустая команда. Пожалуйста, введите команду из списка:");
-                        Console.WriteLine(iCanDo);
-                        Console.WriteLine(separation);
-                        continue;
-                    }
+                if (input == "/exit") break;
 
-                    if (input == "/exit")
+                switch (input)
+                {
+                    case "/start":
+                        StartMessage();
                         break;
-
-                    var userService = new UserService();
-                    var toDoService = new ToDoService(MaxTask, MaxLength);
-                    var updateHandler = new UpdateHandler(userService, toDoService);
-                    updateHandler.HandleUpdateAsync(botClient, update);
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
-                    Console.WriteLine(separation);
-                }
-                {
-
+                    case "/help":
+                        if (!string.IsNullOrEmpty(massege))
+                            Console.WriteLine($"Я с удовольствием напомню тебе, {massege}, что я могу сделать {iCanDo}");
+                        else Console.WriteLine($"Я с удовольствием напомню тебе, что я могу сделать {iCanDo}");
+                        break;
+                    case "/info":
+                        if (!string.IsNullOrEmpty(massege))
+                            Console.WriteLine($"{massege},{info}");
+                        else Console.WriteLine(info);
+                        break;
+                    case "/echo":
+                        if (!string.IsNullOrEmpty(massege))
+                            Console.WriteLine(echo);
+                        else
+                            Console.WriteLine("Сначала представьтесь командой '/start'");
+                        break;
+                    default:
+                        Console.WriteLine("Такой команды не знаю");
+                        break;
                 }
             }
+        }
 
-
-            while (true);
+        public static void StartMessage()
+        {
+            if(!string.IsNullOrEmpty(massege))
+                return;
+            Console.WriteLine("Введите имя:");
+            massege = Console.ReadLine();
+            Console.WriteLine($"Привет, {massege}");
         }
     }
 }
